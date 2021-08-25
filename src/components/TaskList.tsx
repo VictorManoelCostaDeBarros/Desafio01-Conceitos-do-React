a06cbe6cbc2a887123e45e29c4e64193e197710d
Victor Manoel <victor.manoel8@hotmail.com>
2021-08-25 19:38:07 -0300

    :tada: Challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,37 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  function generateId() {
+    const id = Math.floor(Math.random() * (15000 - 1) + 1)
+    const isValid = tasks.filter(t => t.id !== id)
+    if(!isValid) { 
+      generateId()
+    }
+
+    return id;
+  }
+
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return
+
+    setTasks([ ...tasks, { id: generateId(), title: newTaskTitle, isComplete: false} ])
+
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let newTasks = tasks.map(task => task.id === id ? {
+      ...task, 
+      isComplete: !task.isComplete
+    }: task)
+    
+    setTasks([...newTasks])
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const tasksFilter = tasks.filter(t => t.id !== id)
+
+    setTasks([...tasksFilter])
   }
 
   return (
